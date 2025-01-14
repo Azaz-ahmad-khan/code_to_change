86eb7acd4dfd85221cefe497909baf6431efdef5
unknown <azazahmadkhan@gmail.com>
2025-01-14 13:09:41 +0500

    some changes were made

--- bottom_nav.dart
+++ bottom_nav.dart
@@ -11,13 +11,27 @@ class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood_outlined), label: 'recepie'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood_outlined), label: 'recepie')
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.lightBlueAccent,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.red,
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.fastfood_outlined), label: 'recepie'),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              label: 'favourite',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'setting')
+          ]),
     );
   }
 }
